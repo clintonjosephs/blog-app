f163da48734560e005925ef28374ff0223ecfc9b
clintonjosephs <clintonjosephs26@gmail.com>
2022-05-02 16:25:09 +0100

    fixed linters errors

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,8 @@
 class Like < ApplicationRecord
-    belongs_to :post
-    belongs_to :user
+  belongs_to :post
+  belongs_to :user
 
-    def update_likes_counter
-        post.increment!(:LikesCounter)
-    end
+  def update_likes_counter
+    post.increment!(:LikesCounter)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,14 @@
 class Post < ApplicationRecord
-    belongs_to :user
+  belongs_to :user
 
-    has_many :comments
-    has_many :likes
+  has_many :comments
+  has_many :likes
 
-    def recent_comments
-        comments.limit(5).order(created_at: :desc)
-    end
+  def recent_comments
+    comments.limit(5).order(created_at: :desc)
+  end
 
-    def update_post_counter
-        user.increment!(:PostsCounter)
-    end
+  def update_post_counter
+    user.increment!(:PostsCounter)
+  end
 end
