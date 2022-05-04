7db336239c6ba0a34db98aa2616348df02411fc8
clintonjosephs <clintonjosephs26@gmail.com>
2022-05-04 16:19:25 +0100

    added flash messages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,34 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
+    @posts = @user.posts.order(created_at: :desc)
   end
 
   def new
-    @user = User.new
+    @current_user = current_user
+  end
+
+  def create
+    new_post = current_user.posts.build(post_params)
+
+    respond_to do |format|
+      if new_post.save
+        format.html { redirect_to user_posts_path(current_user), notice: 'Post was successfully created.' }
+      else
+        format.html { render :new, notice: 'Post was not created.' }
+      end
+    end
   end
 
   def show
     @post = Post.find(params[:id])
     @comments = @post.comments
     @likes = @post.likes
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:Title, :Text)
   end
 end
