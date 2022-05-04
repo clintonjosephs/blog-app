43db0761fa6fac1ecb432aafa89e5050e9fbff13
clintonjosephs <clintonjosephs26@gmail.com>
2022-05-04 17:43:57 +0100

    implemented comment for current user

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :new, :create, :show]
   end
+
+  resources :posts do
+    resources :comments, only: [:create]
+    resources :likes, only: [:create]
+  end
 end
