af008582bfa6b6d5505c007f8a91463fe3c435d7
clintonjosephs <clintonjosephs26@gmail.com>
2022-05-05 13:20:13 +0100

    added validation to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :posts
   has_many :likes
 
+  validates :Name, presence: true, length: { maximum: 250 }
+  validates :PostsCounter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
