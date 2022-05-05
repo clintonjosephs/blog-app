8c97ed91ff4e6d1cebd0828d759425202439002f
clintonjosephs <clintonjosephs26@gmail.com>
2022-05-05 18:37:26 +0100

    added validation to the comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
+  validates :Text, presence: true, length: { maximum: 500 }
+
   def update_comment_counter
     post.increment!(:CommentsCounter)
   end
