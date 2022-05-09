37439f7d897db50e2656d6b665f9713f37702414
clintonjosephs <clintonjosephs26@gmail.com>
2022-05-09 12:40:10 +0100

    successfully created a new user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
-class ApplicationController < ActionController::Base
-  add_flash_types :danger, :info, :warning, :success, :messages
+# class ApplicationController < ActionController::Base
+#   add_flash_types :danger, :info, :warning, :success, :messages, :notice, :alert
+# end
 
-  def current_user
-    User.first
+class ApplicationController < ActionController::Base
+  add_flash_types :danger, :info, :warning, :success, :messages, :notice, :alert
+
+  protect_from_forgery with: :exception
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:Name, :Bio, :Photo, :password, :password_confirmation, :email) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:Name, :Bio, :Photo, :password, :password_confirmation, :email)}
   end
 end
---
+++ db/migrate/20220509113818_add_default_zero_to_post_counter_in_users.rb
@@ -0,0 +1,5 @@
+class AddDefaultZeroToPostCounterInUsers < ActiveRecord::Migration[7.0]
+  def change
+    change_column_default :users, :PostsCounter, 0
+  end
+end
