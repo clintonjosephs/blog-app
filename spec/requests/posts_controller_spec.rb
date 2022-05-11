38651c64aad4cb647c6b68bbba43f0a2b4896d12
clintonjosephs <clintonjosephs26@gmail.com>
2022-05-11 17:12:53 +0100

    Cross checked tests

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  user = User.includes(:posts).first
+  before(:each) do
+    User.destroy_all
+    m = 4
+    while m > 0
+      @user = FactoryBot.create(:user, PostsCounter: 2)
+      FactoryBot.create(:post, user_id: @user.id)
+      FactoryBot.create(:post, user_id: @user.id)
+      m -= 1
+    end
+    @post = @user.posts.first
+  end
+
   describe 'GET #index' do
-    before(:example) { get("/users/#{user.id}/posts") } # get(:index)
+    before(:example) { get("/users/#{@user.id}/posts") } # get(:index)
 
     it 'successfully gets list of posts for a particular user' do
       expect(response).to have_http_status(:ok)
@@ -19,7 +30,7 @@ RSpec.describe 'Posts', type: :request do
   end
 
   describe 'GET #show' do
-    before(:example) { get("/users/#{user.id}/posts/#{user.posts.first.id}") }
+    before(:example) { get("/users/#{@user.id}/posts/#{@post.id}") }
 
     it 'successfully gets details of post 10 from user 11' do
       expect(response).to have_http_status(:ok)
