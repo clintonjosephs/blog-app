0dec6e1bfb4780105e47d14ceb305a846d369ef5
clintonjosephs <clintonjosephs26@gmail.com>
2022-05-12 13:56:50 +0100

    finalized on api processes

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -11,6 +11,8 @@ class Api::V1::ApiController < ActionController::API
       @current_user = User.find(@decoded[:user_id])
     rescue ActiveRecord::RecordNotFound => e
       render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
     end
   end
 end
