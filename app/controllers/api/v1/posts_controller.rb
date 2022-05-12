1eb039379f692267141171842b047d84509cdd99
clintonjosephs <clintonjosephs26@gmail.com>
2022-05-12 13:32:01 +0100

    fixed linters errors

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,34 +1,37 @@
 module Api
-    module V1
-        class PostsController < ApiController
-            before_action :authorize_request
+  module V1
+    class PostsController < ApiController
+      before_action :authorize_request
 
-            def list_comments
-                user_id = params[:user_id]
-                post_id = params[:post_id]
-                if user_id && post_id
-                    comments = Comment.where(user_id: user_id, post_id: post_id)
-                    render json: {status: 'SUCCESS', message: 'Loaded comments successfully', data: comments}, status: :ok
-                else
-                    render json: {status: 'ERROR', message: 'User or post not found'}, status: :not_found
-                end
-            end
-
-            def add_comment
-                user_id = current_user.id
-                text = params[:text]
-                post_id = params[:post_id]
-                if user_id && text && post_id
-                    comment = Comment.new(user_id: user_id, Text: text, post_id: post_id)
-                    if comment.save
-                        render json: {status: 'SUCCESS', message: 'Saved comment successfully', data: comment}, status: :ok
-                    else
-                        render json: {status: 'ERROR', message: 'Comment not saved', data: comment.errors}, status: :unprocessable_entity
-                    end
-                else
-                    render json: {status: 'ERROR', message: 'Parameters incomplete'}, status: :not_found
-                end
-            end
+      def list_comments
+        user_id = params[:user_id]
+        post_id = params[:post_id]
+        if user_id && post_id
+          comments = Comment.where(user_id: user_id, post_id: post_id)
+          render json: { status: 'SUCCESS', message: 'Loaded comments successfully', data: comments },
+                 status: :ok
+        else
+          render json: { status: 'ERROR', message: 'User or post not found' }, status: :not_found
         end
+      end
+
+      def add_comment
+        user_id = current_user.id
+        text = params[:text]
+        post_id = params[:post_id]
+        if user_id && text && post_id
+          comment = Comment.new(user_id: user_id, Text: text, post_id: post_id)
+          if comment.save
+            render json: { status: 'SUCCESS', message: 'Saved comment successfully', data: comment },
+                   status: :ok
+          else
+            render json: { status: 'ERROR', message: 'Comment not saved', data: comment.errors },
+                   status: :unprocessable_entity
+          end
+        else
+          render json: { status: 'ERROR', message: 'Parameters incomplete' }, status: :not_found
+        end
+      end
     end
+  end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,30 +1,30 @@
 module Api
-    module V1
-        require 'json_web_token'
-        class UsersController < ApiController
-            before_action :authorize_request, except: :login
-            def index
-                users = User.all
-                render json: {status: 'SUCCESS', message: 'Loaded users successfully', data: users}, status: :ok
-            end
+  module V1
+    require 'json_web_token'
+    class UsersController < ApiController
+      before_action :authorize_request, except: :login
+      def index
+        users = User.all
+        render json: { status: 'SUCCESS', message: 'Loaded users successfully', data: users }, status: :ok
+      end
 
-            def login
-                @user = User.find_by_email(params[:email])
-                if @user
-                  token = JsonWebToken.encode(user_id: @user.id)
-                  time = Time.now + 24.hours.to_i
-                  render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
-                                 Name: @user.Name }, status: :ok
-                else
-                  render json: { error: 'unauthorized' }, status: :unauthorized
-                end
-              end
-            
-              private
-            
-              def login_params
-                params.permit(:email, :password)
-              end
+      def login
+        @user = User.find_by_email(params[:email])
+        if @user
+          token = JsonWebToken.encode(user_id: @user.id)
+          time = Time.now + 24.hours.to_i
+          render json: { token: token, exp: time.strftime('%m-%d-%Y %H:%M'),
+                         Name: @user.Name }, status: :ok
+        else
+          render json: { error: 'unauthorized' }, status: :unauthorized
         end
+      end
+
+      private
+
+      def login_params
+        params.permit(:email, :password)
+      end
     end
+  end
 end
